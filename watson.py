6033292c548c42dbf237ccc7585f664385bfabf2
Boardens <31590269+boardens@users.noreply.github.com>
2020-07-20 18:17:12 +0200

    Optimize structure

--- watson.py
+++ watson.py
@@ -18,107 +18,82 @@ from os import path
 from colorama import init, Fore, Back, Style
 
 module_name = "Watson"
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 
 # sherlock directory
-sh_dir = 'cd .. && '
+sh_dir = "cd .. && "
 
 init(convert=True, autoreset=True)
 
-def main():
-	# check if the file exist
-	if path.exists(myFile) is True:
+def send_requests(file, data, lenght, quiet=False):
+	# send requests and catch output
+	for i in range(0, lenght):
+		if quiet:
+			subprocess.call((sh_dir+"sherlock.py "+data[i]), shell=True, stdout=open(os.devnull, 'wb'))
 
-		if args.csv:
+		else:
+			subprocess.call((sh_dir+"sherlock.py "+data[i]), shell=True)
+
+		print(Fore.YELLOW+Style.BRIGHT+"[W~:"+Fore.WHITE+file+Fore.YELLOW+"]"+Style.RESET_ALL+" Infos were stored in '"+Style.BRIGHT+data[i]+".txt"+Style.RESET_ALL+"'")
+
+def method(file, csv_format=False, json_format=False, quiet=False):
+	# check if the file exist
+	if path.exists(file) is True:
+
+		if csv_format:
 			# read file
-			with open(myFile, "r") as f:
+			with open(file, "r") as f:
 				data = [line.rstrip() for line in f]
 				data = data[0].split(",")
 
 			# count items
-			itemCount = len(data)
-			print(Fore.YELLOW+Style.BRIGHT+"[W~:"+Fore.WHITE+myFile+Fore.YELLOW+"]"+Style.RESET_ALL+" {0}".format(itemCount)+" username(s) loaded")
+			data_len = len(data)
+			print(Fore.YELLOW+Style.BRIGHT+"[W~:"+Fore.WHITE+file+Fore.YELLOW+"]"+Style.RESET_ALL+" {0}".format(data_len)+" username(s) loaded")
 
-			# send requests to Sherlock
-			n = itemCount
-			for i in range(0, n):
-				if args.quiet:
-					subprocess.call((sh_dir+"sherlock.py "+data[i]), shell=True, stdout=open(os.devnull, 'wb'))
+		elif json_format:
+			# read file
+			with open(file) as json_file :
+				data = json.load(json_file)
 
-				else:
-					subprocess.call((sh_dir+"sherlock.py "+data[i]), shell=True)
-
-				print(Fore.YELLOW+Style.BRIGHT+"[W~:"+Fore.WHITE+myFile+Fore.YELLOW+"]"+Style.RESET_ALL+" Infos were stored in '"+Style.BRIGHT+data[i]+".txt"+Style.RESET_ALL+"'")
-
-		elif args.json:
-			# check if selected file is a JSON file
-			if not myFile.endswith('.json'):
-				print(Fore.RED+Style.BRIGHT+"[!]"+Style.RESET_ALL+" The selected file is not a JSON file.")
-
-			else:
-				# read file
-				with open(myFile) as json_file :
-					data = json.load(json_file)
-
-				# count items
-				itemCount = len(data)
-				print(Fore.YELLOW+Style.BRIGHT+"[W~:"+Fore.WHITE+myFile+Fore.YELLOW+"]"+Style.RESET_ALL+" {0}".format(itemCount)+" username(s) loaded")
-
-				# send requests to Sherlock
-				n = itemCount
-				for i in range(0, n):
-					if args.quiet:
-						subprocess.call((sh_dir+"sherlock.py "+data[i]), shell=True, stdout=open(os.devnull, 'wb'))
-
-					else:
-						subprocess.call((sh_dir+"sherlock.py "+data[i]), shell=True)
-
-					print(Fore.YELLOW+Style.BRIGHT+"[W~:"+Fore.WHITE+myFile+Fore.YELLOW+"]"+Style.RESET_ALL+" Infos were stored in '"+Style.BRIGHT+data[i]+".txt"+Style.RESET_ALL+"'")
+			# count items
+			data_len = len(data)
+			print(Fore.YELLOW+Style.BRIGHT+"[W~:"+Fore.WHITE+file+Fore.YELLOW+"]"+Style.RESET_ALL+" {0}".format(data_len)+" username(s) loaded")
 
 		else:
 			# count lines
-			itemCount = len(open(myFile).readlines(  ))
-			print(Fore.YELLOW+Style.BRIGHT+"[W~:"+Fore.WHITE+myFile+Fore.YELLOW+"]"+Style.RESET_ALL+" {0}".format(itemCount)+" username(s) loaded")
+			data_len = len(open(file).readlines())
+			print(Fore.YELLOW+Style.BRIGHT+"[W~:"+Fore.WHITE+file+Fore.YELLOW+"]"+Style.RESET_ALL+" {0}".format(data_len)+" username(s) loaded")
 
 			# store each line
-			with open(myFile, "r") as ins:
+			with open(file, "r") as ins:
 				data = []
 				for fLine in ins:
 					data.append(fLine)
-					# Remove carriage return
+					# remove carriage return
 					data = [u.rstrip() for u in data]
 
-			# send requests to Sherlock
-			n = itemCount
-			for i in range(0, n):
-				if args.quiet:
-					subprocess.call((sh_dir+"sherlock.py "+data[i]), shell=True, stdout=open(os.devnull, 'wb'))
-
-				else:
-					subprocess.call((sh_dir+"sherlock.py "+data[i]), shell=True)
-
-				print(Fore.YELLOW+Style.BRIGHT+"[W~:"+Fore.WHITE+myFile+Fore.YELLOW+"]"+Style.RESET_ALL+" Infos were stored in '"+Style.BRIGHT+data[i]+".txt"+Style.RESET_ALL+"'")
+		# send requests to sherlock
+		send_requests(file, data, data_len, quiet)
 
 	else:
-		if myFile != "":
-			print(Fore.RED+Style.BRIGHT+"[!]"+Style.RESET_ALL+" There is no such file named '"+Style.BRIGHT+myFile+Style.RESET_ALL+"' !")
+		if file != "":
+			print(Fore.RED+Style.BRIGHT+"[!]"+Style.RESET_ALL+" There is no such file named '"+Style.BRIGHT+file+Style.RESET_ALL+"'!")
 
 		else:
-			print(Fore.RED+Style.BRIGHT+"[!]"+Style.RESET_ALL+" There is no selected file !")
+			print(Fore.RED+Style.BRIGHT+"[!]"+Style.RESET_ALL+" There is no selected file!")
 
-parser = argparse.ArgumentParser(description='Watson : Sherlock assistant to bulk username research', epilog=f'(Version {__version__})')
 
-parser.add_argument('FILE', help="File containing usernames", action='store')
-parser.add_argument('-q', '--quiet', dest="quiet", help="Quiet Sherlock progression and keep only essential informations.", action="store_true")
-parser.add_argument('--csv', dest="csv", help="Use Comma-Separated Values (CSV) File.", action="store_true")
-parser.add_argument('--json', dest="json", help="Use JavaScript Object Notation (JSON) File.", action="store_true")
+parser = argparse.ArgumentParser(description="Watson: Sherlock assistant to bulk username research", epilog=f"(Version {__version__})")
+
+parser.add_argument("FILE", help="File containing usernames", action='store')
+parser.add_argument("-q", "--quiet", dest="quiet", help="Quiet Sherlock progression and keep only essential informations.", action="store_true")
+parser.add_argument("--csv", dest="csv", help="Use Comma-Separated Values (CSV) File.", action="store_true")
+parser.add_argument("--json", dest="json", help="Use JavaScript Object Notation (JSON) File.", action="store_true")
 
 args = parser.parse_args()
 
-myFile = args.FILE
-
 try:
-	main()
+	method(args.FILE, args.csv, args.json, args.quiet)
 
 except Exception as error:
-	print(Fore.RED+Style.BRIGHT+"[!]"+Style.RESET_ALL+" Error : "+str(error))
+	print(Fore.RED+Style.BRIGHT+"[!] "+Style.RESET_ALL+str(error))
